0f3812763a2f5595244599a3ee7f42f1c2a28313
Usuario <Usuario@192.168.160.251>
2021-10-04 12:59:45 +0200

    Merge origin/front into front
    Conflicts:
    	HiberLibrosMS/src/main/java/com/hiberlibros/HiberLibros/controllers/GeneroController.java
    	HiberLibrosMS/src/main/java/com/hiberlibros/HiberLibros/feign/GeneroFeign.java

--- HiberLibrosMS/src/main/java/com/hiberlibros/HiberLibros/feign/GeneroFeign.java
+++ HiberLibrosMS/src/main/java/com/hiberlibros/HiberLibros/feign/GeneroFeign.java
@@ -1,27 +1,31 @@
 package com.hiberlibros.HiberLibros.feign;
 
+import com.hiberlibros.HiberLibros.dtos.GeneroDto;
 import com.hiberlibros.HiberLibros.entities.Genero;
 import java.util.List;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.cloud.openfeign.SpringQueryMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-/**
- *
- * @author Usuario
- */
+
 @FeignClient(contextId = "sgenero", name="HiberLibrosBack")
-@RequestMapping("/genero")
+@RequestMapping("/generoback")
 public interface GeneroFeign {
     
     @GetMapping //Muestra la lista de generos
     public List<Genero> verGeneros();
     
-//    //@SpringQueryMap
-//     @PostMapping("/guardar")
-//    public void formulario(@SpringQueryMap Genero genero);
+    @PostMapping("/guardar")
+    public String formulario(@SpringQueryMap GeneroDto genero);
     
-//    @GetMapping("/editar")
-//    public GeneroDto editarGenero(@RequestParam Integer id);
+    @GetMapping("/editar")
+    public GeneroDto editarGenero(@RequestParam Integer id);
+    
+    @GetMapping("/borrar")
+    public void borrarGenero(@RequestParam Integer id);
+    
     
 }
