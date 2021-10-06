6e6bfeece0af73747879705388bb8691e0a82795
Usuario <Usuario@192.168.160.251>
2021-10-06 12:57:12 +0200

    Merge origin/front into front
    Conflicts:
    	HiberLibrosMS/src/main/resources/templates/generos/genero.html

--- HiberLibrosMS/src/main/java/com/hiberlibros/HiberLibros/controllers/PreferenciaController.java
+++ HiberLibrosMS/src/main/java/com/hiberlibros/HiberLibros/controllers/PreferenciaController.java
@@ -10,12 +10,10 @@ import com.hiberlibros.HiberLibros.interfaces.IUsuarioService;
 import java.util.HashMap;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -41,45 +39,26 @@ public class PreferenciaController {
     
     @GetMapping
     public Map<String, Object> verPreferencias(String mail) {
+        
         Map<String, Object> m = new HashMap<>();
         Usuario u = usuServ.usuarioRegistrado(mail);
-        m.put("preferencias", prefService.findAll());
+        m.put("preferencias", prefService.findByUsuario(u));
         m.put("generos", serviceGenero.getGeneros());
         m.put("formulario", new Preferencia());
 
         return m;
     }
+    
+     @PostMapping("/guardar")
+    public void anadirPreferencia(Preferencia preferencia) {
+    
+        prefService.addPreferencia(preferencia);
+    }
 
-//    @GetMapping
-//    public String verPreferencias(Model model) {
-//        Usuario u = usuServ.usuarioRegistrado(serviceSeguridad.getMailFromContext());
-//        model.addAttribute("preferencias", prefService.findByUsuario(u));
-//        model.addAttribute("generos", serviceGenero.getGeneros());;
-//        model.addAttribute("formulario", new Preferencia());
-//
-//        return "/preferencias/preferencia";
-//    }
 
-//    @PostMapping("/guardar")
-//    public String anadirPreferencia(Integer id_genero) {
-//        
-//        Usuario u = usuServ.usuarioRegistrado(serviceSeguridad.getMailFromContext());
-//        Genero gen = serviceGenero.encontrarPorId(id_genero);
-//        Preferencia pref = new Preferencia();
-//        pref.setGenero(gen);
-//        pref.setUsuario(u);
-//       
-//        prefService.addPreferencia(pref);
-//
-//        return "redirect:/preferencia";
-//    }
-//
-//    @GetMapping("/borrar/{id}")
-//    public String borrarPreferencia(@PathVariable Integer id) {
-//        
-//        
-//        prefService.borrarPreferencia(id);
-//
-//        return "redirect:/preferencia";
-//    }
+    @GetMapping("/borrar")
+    public void borrarPreferencia(Integer id) {
+        
+        prefService.borrarPreferencia(id);
+    }  
 }
